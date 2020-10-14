e2d35c6a24be7e0100808c92c1d00990ae06e933
Javier Salinas <jsalinaspolo@gmail.com>
2020-10-14 22:53:06 +0200

    Refactor tests using temporal directories from testing package

--- internal/git/git-parser_test.go
+++ internal/git/git-parser_test.go
@@ -2,7 +2,7 @@ package git
 
 import (
 	"fmt"
-	verzion2 "github.com/jsalinaspolo/verzion/internal/verzion"
+	"github.com/jsalinaspolo/verzion/internal/verzion"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"io/ioutil"
@@ -14,10 +14,10 @@ import (
 func TestFromFileTags(t *testing.T) {
 	t.Run("get latest tag version", func(t *testing.T) {
 		input := []byte(`078174542934ec4907a66cf334ed4c4eee744fa9`)
+		tempDir := t.TempDir()
 
-		folder := filepath.Join(os.TempDir(), ".git", "refs", "tags")
+		folder := filepath.Join(tempDir, ".git", "refs", "tags")
 		os.MkdirAll(folder, os.ModePerm)
-		defer os.RemoveAll(folder)
 
 		for i := 1; i < 10; i++ {
 			tmpFile := filepath.Join(folder, fmt.Sprintf("v1.%d.0", i))
@@ -25,13 +25,13 @@ func TestFromFileTags(t *testing.T) {
 			require.NoError(t, err)
 		}
 
-		v, err := FromFileTags(os.TempDir())
+		v, err := FromFileTags(tempDir)
 		require.NoError(t, err)
-		assert.Equal(t, v, verzion2.Verzion{Major: 1, Minor: 9, Patch: 0})
+		assert.Equal(t, v, verzion.Verzion{Major: 1, Minor: 9, Patch: 0})
 	})
 
 	t.Run("fail if there is no tag files", func(t *testing.T) {
-		_, err := FromFileTags(os.TempDir())
+		_, err := FromFileTags(t.TempDir())
 		require.Error(t, err)
 	})
 }
@@ -53,15 +53,18 @@ f57b8f4e053ad955041747c6a43e9d73e36bac4e refs/tags/v1.0.7
 bf2b15cd5b04a142f51adb73ac4601251375bb88 refs/tags/v1.0.9
 ^3e8d1a24ed8d7dc827330be704254ccf5ac95a55`)
 
-		folder := filepath.Join(os.TempDir(), ".git")
+		tempDir := t.TempDir()
+		folder := filepath.Join(tempDir, ".git")
+		os.MkdirAll(folder, os.ModePerm)
+
 		tmpFile := filepath.Join(folder, "packed-refs")
 
 		err := ioutil.WriteFile(tmpFile, input, 0666)
-		defer os.Remove(tmpFile)
-
-		v, err := FromPackedRefs(os.TempDir())
 		require.NoError(t, err)
-		assert.Equal(t, v, verzion2.Verzion{Major: 1, Minor: 0, Patch: 9})
+
+		v, err := FromPackedRefs(tempDir)
+		require.NoError(t, err)
+		assert.Equal(t, v, verzion.Verzion{Major: 1, Minor: 0, Patch: 9})
 	})
 
 	t.Run("parse only tags references ", func(t *testing.T) {
@@ -70,15 +73,16 @@ bf2b15cd5b04a142f51adb73ac4601251375bb88 refs/tags/v1.0.9
 2fe51dbfd9bd6fc66d818cd4ee110ffc9e951d42 refs/remotes/origin/v9.9.9
 b12b761f6332ea6de8b98f69921061b90a39379d refs/tags/v1.0.4
 ^8cc58fe566e5d0ef5cadaf456653ab764f1327fb`)
-		folder := filepath.Join(os.TempDir(), ".git")
+		tempDir := t.TempDir()
+		folder := filepath.Join(tempDir, ".git")
+		os.MkdirAll(folder, os.ModePerm)
 		tmpFile := filepath.Join(folder, "packed-refs")
 
 		err := ioutil.WriteFile(tmpFile, input, 0666)
-		defer os.Remove(tmpFile)
 
-		v, err := FromPackedRefs(os.TempDir())
+		v, err := FromPackedRefs(tempDir)
 		require.NoError(t, err)
-		assert.Equal(t, v, verzion2.Verzion{Major: 1, Minor: 0, Patch: 4})
+		assert.Equal(t, v, verzion.Verzion{Major: 1, Minor: 0, Patch: 4})
 	})
 
 	t.Run("parse only tags references reverse ", func(t *testing.T) {
@@ -88,19 +92,20 @@ b12b761f6332ea6de8b98f69921061b90a39379d refs/tags/v1.0.4
 ^8cc58fe566e5d0ef5cadaf456653ab764f1327fb
 2fe51dbfd9bd6fc66d818cd4ee110ffc9e951d42 refs/remotes/origin/v9.9.9`)
 
-		folder := filepath.Join(os.TempDir(), ".git")
+		tempDir := t.TempDir()
+		folder := filepath.Join(tempDir, ".git")
+		os.MkdirAll(folder, os.ModePerm)
 		tmpFile := filepath.Join(folder, "packed-refs")
 
 		err := ioutil.WriteFile(tmpFile, input, 0666)
-		defer os.Remove(tmpFile)
 
-		v, err := FromPackedRefs(os.TempDir())
+		v, err := FromPackedRefs(tempDir)
 		require.NoError(t, err)
-		assert.Equal(t, v, verzion2.Verzion{Major: 1, Minor: 0, Patch: 4})
+		assert.Equal(t, v, verzion.Verzion{Major: 1, Minor: 0, Patch: 4})
 	})
 
 	t.Run("fail if packed refs does not exist ", func(t *testing.T) {
-		_, err := FromPackedRefs(os.TempDir())
+		_, err := FromPackedRefs(t.TempDir())
 		require.Error(t, err)
 	})
 }
